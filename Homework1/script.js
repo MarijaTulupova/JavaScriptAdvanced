c7a77a7ee06f0f96063d110bc71f2251dc252897
MarijaTulupova <marija.adjieva@gmail.com>
2025-01-20 13:13:35 +0100

    Task 1

---
+++ Homework1/script.js
@@ -0,0 +1,32 @@
+const fetchBtn = document.getElementById("fetchBtn");
+const fetchResult = document.getElementById("fetchResult");
+
+function fetchUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (parsedResult) {
+      fetchResult.style.display = "block";
+      fetchResult.innerHTML = "";
+      console.log(parsedResult);
+      for (const user of parsedResult) {
+        fetchResult.innerHTML += `
+              <p><strong>Name:</strong> ${user.name}</p>
+              <p><strong>Email:</strong> ${user.email}</p>
+              <p><strong>Address:</strong> ${user.address.street}, ${user.address.city}</p>
+              <p><strong>Phone:</strong> ${user.phone}</p>
+              <hr>
+            `;
+      }
+    })
+    .catch(function (error) {
+      console.error("An error occurred:", error);
+      fetchResult.innerHTML = `<p>Failed to load user data. Please try again later.</p>`;
+    });
+}
+
+fetchBtn.addEventListener("click", function () {
+  fetchResult.innerHTML = "";
+  fetchUsers();
+});
